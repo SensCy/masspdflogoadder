235e8712c908e146d018a598b639b9fbbd20a933
Michael Pezzetti <michaelpezzetti@senscy.com>
2026-09-08 11:34:53 -0400

    Align role field in add users modal

--- mod/clientspreadsheet/version.php
+++ mod/clientspreadsheet/version.php
@@ -17,7 +17,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_clientspreadsheet';
-$plugin->version = 2026090803;
+$plugin->version = 2026090804;
 $plugin->requires = 2022112800;
 $plugin->maturity = MATURITY_ALPHA;
-$plugin->release = '0.4.1';
+$plugin->release = '0.4.2';
